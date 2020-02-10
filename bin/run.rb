34afd53edaa397bcbfb9cca888de019910f45e95
bumbot <pkim1295@gmail.com>
2020-02-10 12:37:01 -0500

    added better visual clarity

--- bin/run.rb
+++ bin/run.rb
@@ -5,7 +5,9 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+puts "Movies:"
 show_character_movies(character)
 #
 #
+puts "\nVehicles:"
 show_vehicles_ridden(character)
